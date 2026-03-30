9c1c5fcf0d49b21c3d04848936de3e7c39f56ded
Zoo2-bi <bagjui068@gmail.com>
2026-03-30 15:00:32 +0900

    fix:링크수정

--- portfolio/components/ActivitySection.tsx
+++ portfolio/components/ActivitySection.tsx
@@ -1,18 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import sang from "@/assets/sang.png";
-import All from "@/assets/all.jpg"
+import sang from "..sang.png";
+import All from "..all.jpg";
 
 export default function ActivitySection() {
   return (
     <section className="min-h-screen bg-[#e9e9e9] px-20 py-32">
-
       <div className="mx-auto grid max-w-6xl grid-cols-2 gap-32">
-
         {/* LEFT AREA */}
         <div className="relative pl-16">
-
           {/* LINE */}
           <div className="absolute left-0 top-0 h-full w-1 bg-[#D1BD10]" />
 
@@ -24,7 +21,6 @@ export default function ActivitySection() {
 
           {/* ITEMS */}
           <div className="space-y-14">
-
             <div>
               <h3 className="text-2xl font-bold text-[#355f4a]">
                 풀스택 웹 개발자 양성 교육 5기
@@ -72,13 +68,11 @@ export default function ActivitySection() {
                 웹디자인 및 웹기획을 맡아 외주 작업 진행
               </p>
             </div>
-
           </div>
         </div>
 
         {/* RIGHT AREA */}
         <div className="flex flex-col items-center gap-6 pt-12">
-
           <Image
             src={sang}
             alt="activity"
@@ -94,11 +88,8 @@ export default function ActivitySection() {
             height={420}
             className="rounded-lg border border-gray-300 shadow-md"
           />
-
         </div>
-
       </div>
-
     </section>
   );
 }
